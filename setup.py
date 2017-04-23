c815d659122f03b1a2bd8f8459b76c30e6b00a8d
Anand Chitipothu <anandology@gmail.com>
2017-04-23 10:21:20 +0530

    Bumped version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rorocloud',
-    version='0.1.1',
+    version='0.1.2',
     author='rorodata',
     author_email='rorodata.team@gmail.com',
     packages=find_packages(),
