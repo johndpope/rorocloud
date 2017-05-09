73031761009bd25b09eb767b1b989df56b2a121a
Shaik Sohail Yunus <sohail.yunus@rorodata.com>
2017-05-09 15:14:42 +0530

    move autherization to a different module

--- rorocloud/client.py
+++ rorocloud/client.py
@@ -12,67 +12,14 @@
 from __future__ import print_function
 import sys
 import os
-from os.path import expanduser, join, exists
-
-try:
-    import configparser
-except ImportError:
-    # Python 2
-    import ConfigParser as configparser
-
 import requests
 from . import __version__
 from .utils import logger
+from .auth import FileAuthProvider
 
 config = {
     "ROROCLOUD_URL": "https://rorocloud.rorodata.com/"
 }
-
-class AuthProvider:
-    def get_auth(self):
-        raise NotImplementedError()
-
-    def set_auth(self, email, token):
-        raise NotImplementedError()
-
-class FileAuthProvider:
-    def __init__(self):
-        self._configfile = join(expanduser("~"), ".rorocloudrc")
-        self.auth = self._read_auth()
-
-    def get_auth(self):
-        return self.auth
-
-    def set_auth(self, email, token):
-        self._write_auth(email, token)
-
-    def _read_auth(self):
-        if not exists(self._configfile):
-            return
-
-        p = configparser.ConfigParser()
-        p.read(self._configfile)
-        try:
-            email = p.get("default", "email")
-            token = p.get("default", "token")
-            return (email, token)
-        except configparser.NoOptionError:
-            pass
-
-    def _write_auth(self, email, token):
-        p = configparser.ConfigParser()
-        p.read(self._configfile)
-
-        if not p.has_section("default"):
-            p.add_section("default")
-
-        p.set("default", "email", email)
-        p.set("default", "token", token)
-
-        with open(self._configfile, "w") as f:
-            p.write(f)
-
-        print("Token saved in", self._configfile)
 
 class Client(object):
     """The rorocloud client.
